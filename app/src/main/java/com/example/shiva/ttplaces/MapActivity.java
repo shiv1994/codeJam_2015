90ed6fe559f511318c805399b9a9f2d4e534d21b
shiv1994 <shivramx@gmail.com>
2015-09-30 11:15:18 -0700

    Added photos to map activity.

--- app/src/main/java/com/example/shiva/ttplaces/MapActivity.java
+++ app/src/main/java/com/example/shiva/ttplaces/MapActivity.java
@@ -1,18 +1,28 @@
 package com.example.shiva.ttplaces;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlacePhotoMetadata;
+import com.google.android.gms.location.places.PlacePhotoMetadataBuffer;
+import com.google.android.gms.location.places.PlacePhotoMetadataResult;
+import com.google.android.gms.location.places.PlacePhotoResult;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
@@ -21,11 +31,18 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
 
     private GoogleApiClient mGoogleApiClient;
     int PLACE_PICKER_REQUEST = 1;
+    EditText name,address;
+    ImageView iv;
+    Place place=null;
+    PlacePhotoMetadataBuffer photoMetadataBuffer=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        name = (EditText) findViewById(R.id.editText);
+        address = (EditText) findViewById(R.id.editText2);
+        iv = (ImageView) findViewById(R.id.imageView);
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -104,12 +121,54 @@ public class MapActivity extends AppCompatActivity implements GoogleApiClient.Co
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PLACE_PICKER_REQUEST) {
             if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-                this.finish();
+                place = PlacePicker.getPlace(data, this);
+                setName(place);
+                placePhotosAsync(place);
             }
         }
     }
 
+    public void setName(Place place){
+        name.setText(place.getName());
+        address.setText(place.getAddress());
+    }
+
+
+    private ResultCallback<PlacePhotoResult> mDisplayPhotoResultCallback
+            = new ResultCallback<PlacePhotoResult>() {
+        @Override
+        public void onResult(PlacePhotoResult placePhotoResult) {
+            if (!placePhotoResult.getStatus().isSuccess()) {
+                return;
+            }
+            iv.setImageBitmap(placePhotoResult.getBitmap());
+        }
+    };
+
+
+    private void placePhotosAsync(Place place) {
+        String placeId = place.getId();
+        Places.GeoDataApi.getPlacePhotos(mGoogleApiClient, placeId)
+                .setResultCallback(new ResultCallback<PlacePhotoMetadataResult>() {
+
+
+                    @Override
+                    public void onResult(PlacePhotoMetadataResult photos) {
+                        if (!photos.getStatus().isSuccess()) {
+                            return;
+                        }
+
+                        PlacePhotoMetadataBuffer photoMetadataBuffer = photos.getPhotoMetadata();
+                        if (photoMetadataBuffer.getCount() > 0) {
+                            // Display the first bitmap in an ImageView in the size of the view
+                            Log.i("PHOTO COUNT ", ""+photoMetadataBuffer.getCount());
+                            photoMetadataBuffer.get(0)
+                                    .getScaledPhoto(mGoogleApiClient, iv.getWidth(),
+                                            iv.getHeight())
+                                    .setResultCallback(mDisplayPhotoResultCallback);
+                        }
+                        photoMetadataBuffer.release();
+                    }
+                });
+    }
 }
