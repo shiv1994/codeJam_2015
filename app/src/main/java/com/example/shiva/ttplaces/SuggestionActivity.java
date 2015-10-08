cda17cbf0e56777acd23b76736b7291d195f8694
AlanaJDWarden <warden.alana@gmail.com>
2015-10-08 12:55:11 -0400

    Country of Origin added to the questions

--- app/src/main/java/com/example/shiva/ttplaces/SuggestionActivity.java
+++ app/src/main/java/com/example/shiva/ttplaces/SuggestionActivity.java
@@ -21,10 +21,11 @@ public class SuggestionActivity extends NavDrawer {
     private static final String ANSWER1="ansKey1";
     private static final String ANSWER2="ansKey2";
     private static final String ANSWER3="ansKey3";
+    private static final String ANSWER4="ansKey4";
 
-    String ans1="",ans2="",ans3="";
+    String ans1="",ans2="",ans3="",ans4="";
 
-    Spinner answer1,answer2,answer3;
+    Spinner answer1,answer2,answer3,answer4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,26 +35,70 @@ public class SuggestionActivity extends NavDrawer {
         answer1 = (Spinner) findViewById(R.id.spinner1);
         answer2 = (Spinner) findViewById(R.id.spinner2);
         answer3 = (Spinner) findViewById(R.id.spinner3);
+        answer4 = (Spinner) findViewById(R.id.spinner4);
 
-        //creates 3 arrays to hold answer values for each of the three spinners
+        //creates 4 arrays to hold answer values for each of the 4 spinners
         List<String> decision1 = new ArrayList<>();
-        decision1.add("Yes");
-        decision1.add("A little");
-        decision1.add("No");
+        decision1.add("Antigua and Barbuda");
+        decision1.add("Argentina");
+        decision1.add("Australia");
+        decision1.add("Bahamas");
+        decision1.add("Barbados");
+        decision1.add("Belize");
+        decision1.add("Canada");
+        decision1.add("Cuba");
+        decision1.add("Dominica");
+        decision1.add("Dominican Republic");
+        decision1.add("France");
+        decision1.add("Germany");
+        decision1.add("Grenada");
+        decision1.add("Guyana");
+        decision1.add("Haiti");
+        decision1.add("India");
+        decision1.add("Italy");
+        decision1.add("Jamaica");
+        decision1.add("Japan");
+        decision1.add("Maldives");
+        decision1.add("Mexico");
+        decision1.add("Netherlands");
+        decision1.add("New Zealand");
+        decision1.add("Panama");
+        decision1.add("Peru");
+        decision1.add("Portugal");
+        decision1.add("Russia");
+        decision1.add("St. Kitts and Nevis");
+        decision1.add("St. Lucia");
+        decision1.add("St. Vincent and The Grenadines");
+        decision1.add("Spain");
+        decision1.add("Suriname");
+        decision1.add("Switzerland");
+        decision1.add("Trinidad and Tobago");
+        decision1.add("Turkey");
+        decision1.add("United Arab Emirates");
+        decision1.add("United Kingdom");
+        decision1.add("United States of America");
+        decision1.add("Venezuela");
+        decision1.add("Vietnam");
+        decision1.add("Not Listed");
+
 
         List<String> decision2 = new ArrayList<>();
-        decision2.add("Beach");
-        decision2.add("Museum");
-        decision2.add("Waterfall/River");
-        decision2.add("Historical Landmarks");
-        decision2.add("N/A");
+        decision2.add("Yes");
+        decision2.add("A little");
+        decision2.add("No");
 
         List<String> decision3 = new ArrayList<>();
-        decision3.add("Quiet");
-        decision3.add("Social");
-        decision3.add("Learning");
+        decision3.add("Beach");
+        decision3.add("Museum");
+        decision3.add("Waterfall/River");
+        decision3.add("Historical Landmarks");
         decision3.add("N/A");
 
+        List<String> decision4 = new ArrayList<>();
+        decision4.add("Quiet");
+        decision4.add("Social");
+        decision4.add("Learning");
+        decision4.add("N/A");
 
         ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,decision1);
         dataAdapter1.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
@@ -64,9 +109,14 @@ public class SuggestionActivity extends NavDrawer {
         dataAdapter2.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
         answer2.setAdapter(dataAdapter2);
 
+
         ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,decision3);
         dataAdapter3.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
         answer3.setAdapter(dataAdapter3);
+
+        ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,decision4);
+        dataAdapter4.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        answer4.setAdapter(dataAdapter4);
 
 
     }
@@ -85,6 +135,7 @@ public class SuggestionActivity extends NavDrawer {
         ans1 = answer1.getSelectedItem().toString();
         ans2 = answer2.getSelectedItem().toString();
         ans3 = answer3.getSelectedItem().toString();
+        ans4 = answer4.getSelectedItem().toString();
 
         sharedPreferences = getSharedPreferences(sharedPreferenceName, Context.MODE_PRIVATE);
 
@@ -93,9 +144,22 @@ public class SuggestionActivity extends NavDrawer {
         editor.putString(ANSWER1, ans1);
         editor.putString(ANSWER2, ans2);
         editor.putString(ANSWER3, ans3);
+        editor.putString(ANSWER4, ans4);
         editor.apply();
 
         runMainActivity();
+
+        //suggestPlace();
     }
+
+    /*Suggestions Algorithm*/
+
+    public void suggestPlace(){
+
+
+
+
+    }
+
 }
 
