15f8e29113abe27cba5c4c05c863a47145809421
shiv1994 <shivramx@gmail.com>
2015-10-14 12:51:03 -0700

    Pulling from the online parse database is working for the maps activity.

--- app/src/main/java/com/example/shiva/ttplaces/MapsActivity.java
+++ app/src/main/java/com/example/shiva/ttplaces/MapsActivity.java
@@ -1,9 +1,11 @@
 package com.example.shiva.ttplaces;
 
+import android.app.ProgressDialog;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.shiva.ttplaces.pojo.MyPlace;
 import com.google.android.gms.common.ConnectionResult;
@@ -14,39 +16,78 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity{
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private ArrayList<MyPlace> places;
+    private ArrayList<String> placeIds;
     private LatLng myLoc;
     private boolean locSet = false;
-//    TODO: write a function to get JSON objects from server and populate places arraylist
-
-    //TODO: remove this function when actual function to populate list is implemented
-    private void loadTestPlaces(ArrayList<MyPlace> list){
-        MyPlace mp1 = new MyPlace("Some place in Couva","meh" ,"Couva" , new LatLng(10.555112, -61.320147) );
-        list.add(mp1);
-        MyPlace mp2 = new MyPlace("Nation Museum","meh" ,"POS" , new LatLng(10.662818, -61.510470) );
-        list.add(mp2);
-        MyPlace mp3 = new MyPlace("Maracus Bay","meh" ,"Maracus" , new LatLng(10.758909, -61.436263) );
-        list.add(mp3);
-        MyPlace mp4 = new MyPlace("Fort King George","meh" ,"Scarborough" , new LatLng(11.177967, -60.727075) );
-        list.add(mp4);
-        MyPlace mp5 = new MyPlace("UWI","meh" ,"St Augustine" , new LatLng(10.643435, -61.400062) );
-        list.add(mp5);
-    }
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         places = new ArrayList<>();
-        loadTestPlaces(places);
+        placeIds = new ArrayList<>();
+        loadTestPlaces();
         setUpMapIfNeeded();
+    }
+
+    private void loadTestPlaces(){
+        showProgressDialog("Getting Data");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Place");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    Log.i("Objects Retrieved", "" + objects);
+                    int size = objects.size();
+                    for(int count=0; count <size; count++){
+                        String id = objects.get(count).getObjectId();
+                        placeIds.add(id);
+                    }
+                    loadIndividualPlace();
+                } else {
+                    // objectRetrievalFailed();
+                }
+            }
+        });
+    }
+
+    public void loadIndividualPlace(){
+        for(int i=0 ; i<placeIds.size(); i++){
+            ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Place");
+            query2.getInBackground(placeIds.get(i), new GetCallback<ParseObject>() {
+                public void done(ParseObject object, ParseException e) {
+                    if (e == null) {
+
+                        double latitude = object.getParseGeoPoint("locationLatLong").getLatitude();
+                        double longitude = object.getParseGeoPoint("locationLatLong").getLongitude();
+                        String name = object.getString("Name");
+                        String area = object.getString("Area");
+
+                        MyPlace placeToAdd = new MyPlace(name, "meh" , area , new LatLng(latitude, longitude) );
+                        places.add(placeToAdd);
+                        dismisssProgressDialog();
+                        showMarkers(places);
+                    } else {
+                        // something went wrong
+                    }
+                }
+            });
+        }
     }
 
     @Override
@@ -84,7 +125,7 @@ public class MapsActivity extends FragmentActivity{
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationChangeListener(myLocationChangeListener);
-        showMarkers(places);
+        //showMarkers(places);
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
@@ -103,4 +144,16 @@ public class MapsActivity extends FragmentActivity{
             mMap.addMarker(markerOptions);
         }
     }
+
+    public void showProgressDialog(String message){
+        progressDialog = new ProgressDialog(MapsActivity.this);
+        progressDialog.setMessage(message);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    public void dismisssProgressDialog(){
+        progressDialog.dismiss();
+    }
+
 }
