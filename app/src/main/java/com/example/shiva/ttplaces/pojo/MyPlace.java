e9399e0902de899fdb835c153a4b5f617a46d8c9
AlanaJDWarden <warden.alana@gmail.com>
2015-10-15 01:21:03 -0400

    MyPlace. java updated to match db (except type)

--- app/src/main/java/com/example/shiva/ttplaces/pojo/MyPlace.java
+++ app/src/main/java/com/example/shiva/ttplaces/pojo/MyPlace.java
@@ -10,37 +10,28 @@ public class MyPlace {
     private String name = "";
     private String type = "";
     private String area = "";
-    private int recreationVal;
-    private double rating = 0;
+    private int recreationVal = 0;
+    private int educationalVal = 0;
+    private int religiousVal = 0;
+    private int remoteVal = 0;
+    //private double rating = 0;
 
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public MyPlace(String name, String type, String area, LatLng position) {
+    public MyPlace(String name, String type, String area, LatLng position, int recreationVal, int educationalVal, int religiousVal, int remoteVal) {
         this.name = name;
         this.type = type;
-        this.position = position;
         this.area = area;
+        this.position = position;
+        this.recreationVal = recreationVal;
+        this.educationalVal = educationalVal;
+        this.religiousVal = religiousVal;
+        this.remoteVal = remoteVal;
     }
 
     public LatLng getPosition() {
         return position;
     }
 
-    public void setLatLng(LatLng position) {
+    public void setPosition(LatLng position) {
         this.position = position;
     }
 
@@ -59,5 +50,44 @@ public class MyPlace {
     public void setType(String type) {
         this.type = type;
     }
-}
 
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public int getRecreationVal() {
+        return recreationVal;
+    }
+
+    public void setRecreationVal(int recreationVal) {
+        this.recreationVal = recreationVal;
+    }
+
+    public int getEducationalVal() {
+        return educationalVal;
+    }
+
+    public void setEducationalVal(int educationalVal) {
+        this.educationalVal = educationalVal;
+    }
+
+    public int getReligiousVal() {
+        return religiousVal;
+    }
+
+    public void setReligiousVal(int religiousVal) {
+        this.religiousVal = religiousVal;
+    }
+
+    public int getRemoteVal() {
+        return remoteVal;
+    }
+
+    public void setRemoteVal(int remoteVal) {
+        this.remoteVal = remoteVal;
+    }
+}
