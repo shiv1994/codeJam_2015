d3cdf669a0c004e1b2d66f909d05cd7f3ff25cf4
shiv1994 <shivramx@gmail.com>
2015-10-14 21:30:43 -0700

    Maps activity to load places/ beacons onto map working!

--- app/src/main/java/com/example/shiva/ttplaces/MapsActivity.java
+++ app/src/main/java/com/example/shiva/ttplaces/MapsActivity.java
@@ -1,22 +1,20 @@
 package com.example.shiva.ttplaces;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.location.Location;
-import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.shiva.ttplaces.pojo.MyPlace;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.FindCallback;
@@ -34,21 +32,26 @@ public class MapsActivity extends FragmentActivity{
     private ArrayList<String> placeIds;
     private LatLng myLoc;
     private boolean locSet = false;
+    String classToPull = null;
+    private CameraUpdate myLocation;
     ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        classToPull="Place";
+        runAlertDialog();
         places = new ArrayList<>();
         placeIds = new ArrayList<>();
-        loadTestPlaces();
+//        loadTestPlaces();
         setUpMapIfNeeded();
     }
 
     private void loadTestPlaces(){
+
         showProgressDialog("Getting Data");
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Place");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery(classToPull);
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
@@ -68,7 +71,7 @@ public class MapsActivity extends FragmentActivity{
 
     public void loadIndividualPlace(){
         for(int i=0 ; i<placeIds.size(); i++){
-            ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Place");
+            ParseQuery<ParseObject> query2 = ParseQuery.getQuery(classToPull);
             query2.getInBackground(placeIds.get(i), new GetCallback<ParseObject>() {
                 public void done(ParseObject object, ParseException e) {
                     if (e == null) {
@@ -81,8 +84,10 @@ public class MapsActivity extends FragmentActivity{
                         MyPlace placeToAdd = new MyPlace(name, "meh" , area , new LatLng(latitude, longitude) );
                         places.add(placeToAdd);
                         dismisssProgressDialog();
+                        //We would like to show the places once the pulling from the internet is complete.
                         showMarkers(places);
-                    } else {
+                    }
+                    else {
                         dismisssProgressDialog();
                     }
                 }
@@ -122,10 +127,17 @@ public class MapsActivity extends FragmentActivity{
         }
     };
 
+    private void setDefaultLocationAndZoom(){
+        LatLng triniTob = new LatLng(10.450602, -61.244437);
+        myLocation = CameraUpdateFactory.newLatLngZoom(triniTob,10);
+    }
+
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
-        mMap.setOnMyLocationChangeListener(myLocationChangeListener);
-        //showMarkers(places);
+       // mMap.setOnMyLocationChangeListener(myLocationChangeListener);
+        //showMarkers(places)
+        setDefaultLocationAndZoom();
+        mMap.animateCamera(myLocation);
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
@@ -156,4 +168,23 @@ public class MapsActivity extends FragmentActivity{
         progressDialog.dismiss();
     }
 
-}
+    public void runAlertDialog(){
+        AlertDialog alert = new AlertDialog.Builder(MapsActivity.this).create();
+        alert.setTitle("Reset...");
+        alert.setMessage("Are you sure?");
+        alert.setButton(Dialog.BUTTON_POSITIVE, "View Beacons", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                classToPull="Beacon";
+                loadTestPlaces();
+            }
+        });
+        alert.setButton(Dialog.BUTTON_NEGATIVE, "View Places", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                classToPull="Place";
+                loadTestPlaces();
+            }
+        });
+        alert.show();
+        }
+
+    }
