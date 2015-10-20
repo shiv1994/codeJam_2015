4ca8ace24c64f31ccb5d1f2b098edc952fe1289c
AlanaJDWarden <warden.alana@gmail.com>
2015-10-19 22:56:00 -0400

    documentation

--- app/src/main/java/com/example/shiva/ttplaces/pojo/TabsPagerAdapter.java
+++ app/src/main/java/com/example/shiva/ttplaces/pojo/TabsPagerAdapter.java
@@ -1,3 +1,6 @@
+//in tour activity, there are 4 fragments: audio, text, images and videos
+//
+
 package com.example.shiva.ttplaces.pojo;
 
 import android.os.Bundle;
@@ -23,22 +26,22 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
         b = new Bundle();
         b.putString("url",item.getUrl());
 
-		if(item.type==0) {
+		if(item.type==0) { //type 0 represents audio
             AudioFragment audio = new AudioFragment();
             audio.setArguments(b);
             return audio;
         }
-		else if(item.type==1) {
+		else if(item.type==1) { //type 1 represents text
             TextFragment text = new TextFragment();
             text.setArguments(b);
             return text ;
         }
-        else if(item.type==2) {
+        else if(item.type==2) { //type 2 represents image
             ImageFragment image =new ImageFragment();
             image.setArguments(b);
             return image;
         }
-		else if(item.type==3) {
+		else if(item.type==3) { //type 3 represents video
             VideoFragment video = new VideoFragment();
             video.setArguments(b);
             return video ;
@@ -47,8 +50,8 @@ public class TabsPagerAdapter extends FragmentPagerAdapter {
 		return null;
 	}
 	@Override
-	public int getCount(){
-		// number of tabs
+	public int getCount(){ //the number of tabs indicate the number of fragments
+		// number of tab is dynamic, in that depending on how many types there are, a new fragment is created
 		return tours.size();
 	}
 
